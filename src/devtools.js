11a3a554ad4c01842df0a0cce851645e9caae60d
Ben Robinson <ben.robinson@bbc.co.uk>
2017-04-28 11:30:35 +0100

    Clean up old files

--- src/devtools.js
+++ src/devtools.js
@@ -1,12 +1,5 @@
-chrome.devtools.panels.create("VideoContext",
+chrome.devtools.panels.create(
+    "VideoContext",
     "icon.png",
-    "panel/index.html",
-    function(panel) {
-      // code invoked on panel creation
-    //   console.log("i made a panel");
-      setInterval(() => {
-        //   console.log("some regular task");
-        //   console.log(panel);
-      }, 1000)
-    }
+    "panel/index.html"
 );
