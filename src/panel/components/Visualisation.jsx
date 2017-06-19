c61b2bde590d443bfaf03b447be5c5e6cbf849db
Ben Robinson <ben.robinson@bbc.co.uk>
2017-06-19 15:17:25 +0100

    Begin showing more info
    Play state etc

--- src/panel/components/Visualisation.jsx
+++ src/panel/components/Visualisation.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 import VideoContextVisualisation from '@bbc/visualise-videocontext'
 import './Visualisation.css'
 
+const convertStateEnum = (num) => {
+    switch (num) {
+    case 0:
+        return 'Playing'
+    case 1:
+        return 'Paused'
+    case 2:
+        return 'Stalled'
+    case 3:
+        return 'Ended'
+    case 4:
+        return 'Broken'
+    default:
+        return `ERROR: unknown state with code ${num}`
+    }
+}
+
 export default class Visualisation extends React.Component {
     componentDidMount () {
         this._vis = new VideoContextVisualisation(this._ref)
@@ -17,15 +34,34 @@ export default class Visualisation extends React.Component {
     }
     render () {
         return (
-            <div
-                styleName="vis"
-                onMouseDown={this.props.onZoom}
-                onWheel={this.props.onZoom}
-            >
+            <div styleName="main">
                 <div
-                    ref={(ref) => { this._ref = ref }}
                     styleName="vis"
-                />
+                    onMouseDown={this.props.onZoom}
+                    onWheel={this.props.onZoom}
+                >
+                    <div
+                        ref={(ref) => { this._ref = ref }}
+                        styleName="rendering"
+                    />
+                </div>
+                <div styleName="other-info">
+                    <table><tbody>
+                        <tr>
+                            <td>Current time:</td>
+                            <td>{this.props.json.videoContext.currentTime}s</td>
+                        </tr>
+                        <tr>
+                            <td>Duration:</td>
+                            <td>{this.props.json.videoContext.duration}s</td></tr>
+                        <tr>
+                            <td>State:</td>
+                            <td>{convertStateEnum(this.props.json.videoContext.state)}</td></tr>
+                        <tr>
+                            <td>Playback rate:</td>
+                            <td>{this.props.json.videoContext.playbackRate}</td></tr>
+                    </tbody></table>
+                </div>
             </div>
         )
     }
